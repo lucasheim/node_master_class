ca230cab544df6c044e6475562e0eddc9df71858
lucasheim <lucasheim1983@gmail.com>
2018-09-07 22:04:49 -0300

    Adding https support

--- restful_api/index.js
+++ restful_api/index.js
@@ -2,13 +2,38 @@
 
 // Dependencies
 var http = require('http');
+var https = require('https');
 var url = require('url');
 var StringDecoder = require('string_decoder').StringDecoder;
 var config = require('./config');
+var fs = require('fs');
 
-// The server should respond to all requests with a string
-var server = http.createServer(function(req, res) {
+// Instatiating the http server
+var httpServer = http.createServer(function(req, res) {
+    unifiedServer(req, res);
+});
 
+// Instatiating the https server
+var httpsServerOptions = {
+    "key": fs.readFileSync("./https/key.pem"),
+    "cert": fs.readFileSync("./https/cert.pem")
+}; 
+var httpsServer = https.createServer(httpsServerOptions, function(req, res) {
+    unifiedServer(req, res);
+});
+
+// Start the http server
+httpServer.listen(config.httpPort, function() {
+    console.log("The server is listening on port " + config.httpPort);
+});
+
+// Start the https server
+httpsServer.listen(config.httpsPort, function() {
+    console.log("The server is listening on port " + config.httpsPort);
+});
+
+// All the server logic for both the http and https server
+var unifiedServer = function(req, res) {
     // Get the URL and parse it
     var parsedUrl = url.parse(req.url, true);
 
@@ -66,12 +91,7 @@ var server = http.createServer(function(req, res) {
             console.log("Returning this response: ", statusCode, payloadString);
         });
     });
-});
-
-// Start the server
-server.listen(config.port, function() {
-    console.log("The server is listening on port " + config.port + " in "+ config.envName + " mode");
-});
+};
 
 // Define the handlers
 var handlers = {};
